8b1fad7dba6eaf374d14bb825dfdf78a0dd4dffa
Henrique Paiva <hrpaiva3@gmail.com>
2025-12-13 08:53:49 -0300

    Corrige a estrutura de pastas do projeto e adiciona as migrations de usuario

--- eventsync_ai/settings.py
+++ eventsync_ai/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from decouple import config
+from datetime import timedelta
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -125,4 +126,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 STATIC_URL = 'static/'
 
-AUTH_USER_MODEL = 'users.User'
+AUTH_USER_MODEL = 'users.Usuario'
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
